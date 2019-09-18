3ddda78389dc66d924a641868a4048b5ee1f8475
MattisTrygstad <mattislt@sf-nett.no>
2019-09-18 11:13:08 +0200

    site layout and alternative buttons

--- exhibition-app/src/components/app/App.js
+++ exhibition-app/src/components/app/App.js
@@ -6,20 +6,34 @@ export class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      categoryValues: [(0,"category_1"), (1,"category_2"), (2,"category_3")]
+      categoryValues: [(0,"category_1"), (1,"category_2"), (2,"category_3")],
+      alternatives: [(0,"1"), (1,"2"), (2,"3"), (3,"4")]
     }
 }
 render(){
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="flex_container">
+          <div className="header">
+            <h1>Placeholder</h1>
+          </div>
+          <div className="categories">
           <p>Images</p>
           < ButtonContainer values={this.state.categoryValues} />
           <p>Text</p>
           < ButtonContainer values={this.state.categoryValues}/>
           <p>Sound</p>
           < ButtonContainer values={this.state.categoryValues}/>
-        </header>
+          <p>Alternatives</p>
+          < ButtonContainer values={this.state.alternatives} />
+          </div>
+          <div className="exhibition">
+            <p>placeholder</p>
+          </div>
+          <div className="footer">
+            <p>footer</p>
+          </div>
+        </div>
       </div>
     );
   }
