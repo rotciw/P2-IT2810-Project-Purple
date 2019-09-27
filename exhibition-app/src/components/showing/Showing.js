e59dfd9b5c625c5f4da2bcac81f4d5c905bf6370
MattisTrygstad <mattislt@sf-nett.no>
2019-09-27 15:00:17 +0200

    removed console logs

--- exhibition-app/src/components/showing/Showing.js
+++ exhibition-app/src/components/showing/Showing.js
@@ -14,14 +14,12 @@ export default class Showing extends Component {
 fetchImage(imageCategory, imageNumber){
     if (sessionStorage.getItem("image" + (imageCategory+1) + "_" + (imageNumber+1)) !== null){
         this.setState({image: sessionStorage.getItem("image" + (imageCategory+1) + "_" + (imageNumber+1))});
-        console.log("Loaded image from sessionStorage");
     }else{
         fetch("assets/img/image" + (imageCategory+1) + "_" + (imageNumber+1) + ".svg")
         .then(response => response.text())
         .then(str => {
             this.setState({image: str});
             sessionStorage.setItem("image" + (imageCategory+1) + "_" + (imageNumber+1), str);
-            console.log("Fetched image using AJAX Fetch API");
         })
     }
 }
@@ -30,7 +28,6 @@ fetchPoem(poemCategory, poemNumber) {
     if (sessionStorage.getItem("poemTitle" + (poemCategory+1) + "_" + (poemNumber+1)) !== null){
         this.setState({poemTitle: sessionStorage.getItem("poemTitle" + (poemCategory+1) + "_" + (poemNumber+1))});
         this.setState({poemContent: sessionStorage.getItem("poemContent" + (poemCategory+1) + "_" + (poemNumber+1))});
-        console.log("Loaded poem from sessionStorage");
     }else{
         fetch("assets/text/poems" + (poemCategory+1) + ".json")
         .then(response => {
@@ -44,7 +41,6 @@ fetchPoem(poemCategory, poemNumber) {
                 })
                 sessionStorage.setItem("poemTitle" + (poemCategory+1) + "_" + (poemNumber+1), result.poems[poemNumber].title);
                 sessionStorage.setItem("poemContent" + (poemCategory+1) + "_" + (poemNumber+1), result.poems[poemNumber].content);
-                console.log("Fetched poem using AJAX Fetch API");
             }
         ).catch(err => {
             console.log("Error: " + err)
@@ -55,14 +51,11 @@ fetchAudio(audioCategory, audioNumber) {
     if (sessionStorage.getItem("audio" + (audioCategory+1) + "_" + (audioNumber+1)) !== null){
         let audioJSON = JSON.parse(sessionStorage.getItem("audio" + (audioCategory+1) + "_" + (audioNumber+1)));
         let audioElement = React.createElement('audio',{style:{width:"100%", outline:"none"},src:audioJSON.props.src, type:'audio/mpeg', controls:true},null)
-        console.log(audioJSON.props.src)
         this.setState({audio: audioElement});
-        console.log("Loaded audio from sessionStorage");
     }else{
         let element = React.createElement('audio',{style:{width:"100%", outline:"none"}, className:"test",src:('assets/audio/audio' + (audioCategory+1) + '_' + (audioNumber+1) + '.mp3'), type:'audio/mpeg', controls:true},null)
         this.setState({audio: element})
         sessionStorage.setItem("audio" + (audioCategory+1) + "_" + (audioNumber+1), JSON.stringify(element))
-        console.log("Fetched audio using AJAX Fetch API");
     }
 }
 
